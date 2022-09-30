7d1a511a6938a5bde6114d44eeca5b07186c692f
unknown <akikiun14@gmail.com>
2022-09-30 15:45:02 +0800

    initial upload

--- PHP array exercise 03.php
+++ PHP array exercise 03.php
@@ -1,13 +1,14 @@
 <?php
+$counter=0;
 $ArrayLength = (int)readline('Input the number of elements to be stored in the array :');
 error_reporting(E_ERROR | E_PARSE);
 echo "Input ".$ArrayLength." elements in the array :";
 $array = range($ArrayLength);
 echo "\n";
 for ($i = 0; $i < $ArrayLength; $i++) {
-     $elements= (int)readline('element: ');
+     $elements= (int)readline('element - '.$counter. ': ');
       $array[]=$elements;
-      
+      $counter++;
 }
 $oddArray = array();
 $evenArray = array();
